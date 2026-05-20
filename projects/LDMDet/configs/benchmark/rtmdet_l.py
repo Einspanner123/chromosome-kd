10362b2124ffdc3eb2b69531d2034387ab602159
linkst <2024023709@m.scnu.edu.cn>
2026-05-20 11:01:03 +0800

    fix(benchmark): update RTMDet-L pretrained checkpoint URL
    The previous URL (cspnext_r50_320) returned 404. Updated to use the
    correct CSPNeXt-l ImageNet pretrained weights with prefix='backbone.'
    from cspnext_rsb_pretrain directory.

--- projects/LDMDet/configs/benchmark/rtmdet_l.py
+++ projects/LDMDet/configs/benchmark/rtmdet_l.py
@@ -30,7 +30,8 @@ model = dict(
         act_cfg=dict(type='SiLU', inplace=True),
         init_cfg=dict(
             type='Pretrained',
-            checkpoint='https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_r50_320-2a9b6c31.pth',
+            prefix='backbone.',
+            checkpoint='https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_rsb_pretrain/cspnext-l_8xb256-rsb-a1-600e_in1k-6a760974.pth',
         ),
     ),
     neck=dict(
