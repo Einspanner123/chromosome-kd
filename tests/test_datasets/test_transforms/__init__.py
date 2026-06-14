a5ef573add6cf5f667a1f0bdae2290446948abb0
linkst <2024023709@m.scnu.edu.cn>
2026-06-14 12:46:28 +0800

    chore: remove mmdet upstream tests and dev scripts
    These are mmdet framework test suites and development scripts
    not relevant to the LDMDet project.

--- tests/test_datasets/test_transforms/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .utils import construct_toy_data, create_full_masks, create_random_bboxes
-
-__all__ = ['construct_toy_data', 'create_full_masks', 'create_random_bboxes']
--- tests/test_models/test_backbones/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .utils import check_norm_state, is_block, is_norm
-
-__all__ = ['check_norm_state', 'is_block', 'is_norm']
