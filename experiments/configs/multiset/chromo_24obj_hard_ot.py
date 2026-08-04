2cf160cf37d2861767de1fcedad86f6c67d9f045
linkst <2024023709@m.scnu.edu.cn>
2026-08-04 20:33:21 +0800

    fix: 为 D2 Hard OT config 添加 test_dataloader/test_evaluator 覆盖
    原配置仅覆盖 train/val, test 继承 D1 base (指向 D1 test 数据)。
    添加 test_dataloader (D2 test, 1000 imgs) + test_evaluator (format_only=False),
    与 test_merged_on_24obj.py 模式对齐, 支持 --dataset test 评估。

--- experiments/configs/multiset/chromo_24obj_hard_ot.py
+++ experiments/configs/multiset/chromo_24obj_hard_ot.py
@@ -5,3 +5,18 @@ data_root = 'data/24_chromosomes_object/coco/'
 train_dataloader = dict(dataset=dict(data_root=data_root, ann_file='train/_annotations.coco.json', data_prefix=dict(img='train/')))
 val_dataloader = dict(dataset=dict(data_root=data_root, ann_file='valid/_annotations.coco.json', data_prefix=dict(img='valid/')))
 val_evaluator = dict(ann_file=data_root + 'valid/_annotations.coco.json')
+test_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        ann_file='test/_annotations.coco.json',
+        data_prefix=dict(img='test/'),
+        test_mode=True,
+    ),
+)
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'test/_annotations.coco.json',
+    metric='bbox',
+    format_only=False,
+)
